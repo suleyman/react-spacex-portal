0a750d7c1c05e3b784446c800fe204038bf3b757
suleymandtr <slymnd28@gmail.com>
2020-12-12 22:41:02 +0300

    updated: formatted date & styling

--- src/components/Launch/Launch.tsx
+++ src/components/Launch/Launch.tsx
@@ -1,5 +1,6 @@
-import React, { FunctionComponent } from "react";
+import { FunctionComponent } from "react";
 import { Link } from "react-router-dom";
+import { format } from "date-fns";
 import { ILaunch } from "../../interfaces/ILaunch";
 import styles from "./Launch.module.css";
 
@@ -8,13 +9,19 @@ type LaunchProps = {
 };
 
 const Launch: FunctionComponent<LaunchProps> = ({ launchData }) => {
-  const { id, name, details } = launchData;
+  const { id, name, details, date_utc } = launchData;
   return (
     <Link to={`/detail/${id}`} className={styles.launch}>
       <h5 className="name">{name}</h5>
-      <h6 className="description">{details}</h6>
+      {details && <h6 className={styles.description}>{details}</h6>}
+      <span className={styles.date}>{formatDate(date_utc)}</span>
     </Link>
   );
 };
+
+function formatDate(date: string): string {
+  const dateFromString = new Date(date);
+  return format(dateFromString, "MM/dd/yyyy");
+}
 
 export default Launch;
