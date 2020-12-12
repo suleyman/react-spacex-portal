ef081a399289a89afc9784d79c088a0412a558df
suleymandtr <slymnd28@gmail.com>
2020-12-13 01:43:13 +0300

    updated: launch card styles

--- src/components/Launch/Launch.tsx
+++ src/components/Launch/Launch.tsx
@@ -12,14 +12,14 @@ const Launch: FunctionComponent<LaunchProps> = ({ launchData }) => {
   const { id, name, details, date_utc, success, upcoming } = launchData;
   return (
     <Link to={`/detail/${id}`} className={styles.launch}>
-      <h5 className="name">
-        {name}
+      <div className={styles.header}>
+        <h5 className={styles.name}>{name}</h5>
         {success && <span className="ms-4 badge rounded-pill bg-success">Success</span>}
         {!success && !upcoming && <span className="ms-4 badge rounded-pill bg-danger">Failed</span>}
         {upcoming && <span className="ms-4 badge rounded-pill bg-primary">Upcoming</span>}
-      </h5>
+        <span className={styles.date}>{formatDate(date_utc)}</span>
+      </div>
       {details && <h6 className={styles.description}>{details}</h6>}
-      <span className={styles.date}>{formatDate(date_utc)}</span>
     </Link>
   );
 };
