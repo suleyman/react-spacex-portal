2714cbd141b23b36a97a53e28931e55144012fc0
suleymandtr <slymnd28@gmail.com>
2020-12-13 00:00:55 +0300

    updated: show loading before fetch data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,28 +2,36 @@ import React, { FunctionComponent, useState, useEffect } from "react";
 import { ILaunch } from "../interfaces/ILaunch";
 import LaunchService from "../api/services/launch";
 import Launch from "../components/Launch/Launch";
+import Loading from "../components/Loading/Loading";
 
 const IndexPage: FunctionComponent = () => {
   const [launches, setLaunches] = useState<ILaunch[]>([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     LaunchService.getAllLaunches().then((response) => {
       setLaunches(response.data);
+      setLoading(false);
     });
   }, []);
 
   return (
     <div className="page-index-page">
       <div className="container">
-        <div className="launches row">
-          {launches.map((launch) => {
-            return (
-              <div className="col-lg-12 mb-4" key={launch.id}>
-                <Launch launchData={launch} />
-              </div>
-            );
-          })}
-        </div>
+        {loading ? (
+          <Loading />
+        ) : (
+          <div className="launches row">
+            {launches.map((launch) => {
+              return (
+                <div className="col-lg-12 mb-4" key={launch.id}>
+                  <Launch launchData={launch} />
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     </div>
   );
